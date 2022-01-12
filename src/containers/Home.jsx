23e4664ed3ab121b329339344e8e3d754d3b67a4
James Troughton <jamesstroughton@gmail.com>
2022-01-13 11:52:24 +1300

    ✨ Home

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,13 +1,28 @@
 import React from "react"
 import Layout from "../components/Layout"
+import { Helmet } from "react-helmet"
+import Header from "../components/Header"
 
 const Home = () => {
   return (
-    <Layout>
-      <div>
-        <h1>Home</h1>
-      </div>
-    </Layout>
+    <>
+      <Layout>
+        <Helmet>
+          <title>James Troughton</title>
+          <meta
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+          />
+        </Helmet>
+        <div
+          className="main-container"
+          id="main-container"
+          data-scroll-container
+        >
+          <Header />
+        </div>
+      </Layout>
+    </>
   )
 }
 
