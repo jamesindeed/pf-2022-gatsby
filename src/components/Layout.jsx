d3dfef793473fc58a1dc327ffc329f1ba3c2a719
James Troughton <jamesstroughton@gmail.com>
2022-01-13 11:51:43 +1300

    ✨ Custom Cursor

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,8 +1,11 @@
 import React from "react"
+import "../styles/globals.scss"
+import CustomCursor from "../CustomCursor"
 
 const Layout = ({ children }) => {
   return (
     <>
+      <CustomCursor />
       <main>{children}</main>
     </>
   )
