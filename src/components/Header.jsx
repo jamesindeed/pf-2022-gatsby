d8fae81f034d32ff95f917bdab61abb8f6f6ccae
James Troughton <jamesstroughton@gmail.com>
2022-01-13 11:51:55 +1300

    ✨ Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import "../styles/header.scss"
+
+const Header = () => {
+  return (
+    <div className="navbar" data-scroll-section>
+      <div>menu</div>
+
+      <div className="navbar-logo">J|T</div>
+
+      <div>blog</div>
+    </div>
+  )
+}
+
+export default Header
