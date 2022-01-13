6fd042a9a17ddee1640d09b7ad5e07660e818ed5
James Troughton <jamesstroughton@gmail.com>
2022-01-14 11:27:51 +1300

    ✨ Locomotive Scroll + Preloader

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,10 +1,38 @@
-import React from "react"
+import React, { useState, useRef, useEffect } from "react"
 import Layout from "../components/Layout"
 import { Helmet } from "react-helmet"
-import Header from "../components/Header"
-import Hero from "../components/Hero"
+import { Header, Hero, Featured } from "../components"
+import useLocoScroll from "../hooks/useLocoScroll"
 
 const Home = () => {
+  const [preloader, setPreloader] = useState(true)
+
+  useLocoScroll(!preloader)
+  const [timer, setTimer] = useState(3)
+
+  const id = useRef(null)
+
+  const clear = () => {
+    window.clearInterval(id.current)
+    setPreloader(false)
+  }
+
+  useEffect(() => {
+    id.current = window.setInterval(() => {
+      setTimer(timer => timer - 1)
+    }, 1000)
+  }, [])
+
+  useEffect(() => {
+    if (timer === 0) {
+      clear()
+    }
+  }, [timer])
+
+  const handlePreloader = () => {
+    setPreloader(false)
+  }
+
   return (
     <>
       <Layout>
@@ -15,14 +43,26 @@ const Home = () => {
             content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
           />
         </Helmet>
-        <div
-          className="main-container"
-          id="main-container"
-          data-scroll-container
-        >
-          <Header />
-          <Hero />
-        </div>
+        {preloader ? (
+          <div
+            onClick={handlePreloader}
+            className="loader-wrapper absolute-class"
+          >
+            <h2>{timer}</h2>
+            <h1>James Troughton</h1>
+            <h2>Auckland, New Zealand</h2>
+          </div>
+        ) : (
+          <div
+            className="main-container"
+            id="main-container"
+            data-scroll-container
+          >
+            <Header />
+            <Hero />
+            <Featured />
+          </div>
+        )}
       </Layout>
     </>
   )
