57c1f189adc754063d1bbee634c1ee7077279190
James Troughton <jamesstroughton@gmail.com>
2022-01-13 14:30:10 +1300

    💫 Hero Animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,30 @@
-import React from "react"
+import React, { useEffect } from "react"
+
+import gsap from "gsap"
+import SplitText from "../utils/split-text.min"
 import "../styles/hero.scss"
 
 const Hero = () => {
+  useEffect(() => {
+    const split = new SplitText("#hero-text", {
+      type: "lines",
+      linesClass: "lineChildren",
+    })
+
+    const splitParent = new SplitText("#hero-text", {
+      type: "lines",
+      linesClass: "lineParent",
+    })
+
+    gsap.to(split.lines, {
+      duration: 1,
+      y: 0,
+      opacity: 1,
+      stagger: 0.1,
+      ease: "power2",
+    })
+  }, [])
+
   return (
     <section className="hero-container" data-scroll-section>
       <ul className="hero-menu">
