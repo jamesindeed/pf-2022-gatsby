7072231bcf30e86cb2b1d7a4c9d00169908318c2
James Troughton <jamesstroughton@gmail.com>
2022-01-25 16:00:03 +1300

    🎨 Layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import "../styles/globals.scss"
+import "../styles/locomotive-scroll.scss"
 import CustomCursor from "../CustomCursor"
 
 const Layout = ({ children }) => {
