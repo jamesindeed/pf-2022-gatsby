f9700fe308293d7941a450edcc3bdc294f44196e
James Troughton <jamesstroughton@gmail.com>
2022-03-20 19:01:24 +1300

    🎨

--- src/components/MobileMessage.jsx
+++ src/components/MobileMessage.jsx
@@ -1,9 +1,17 @@
-import React from "react"
+import React, { useEffect } from "react"
 import useWindowSize from "../hooks/useWindowSize"
 import "../styles/mobileMessage.scss"
 
-const MobileMessage = () => {
+const MobileMessage = ({ location }) => {
   const { width } = useWindowSize()
+
+  useEffect(() => {
+    if (width < 481) {
+      document.body.style.overflow = "none"
+    } else {
+      document.body.style.overflow = "auto"
+    }
+  }, [width, location])
 
   if (width < 481) {
     return (
