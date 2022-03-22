56b0816ad5171b8d11ade021da532eab9c10e802
James Troughton <jamesstroughton@gmail.com>
2022-03-22 20:43:45 +1300

    🔥

--- gatsby-ssr.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-import { Layout } from "./src/components"
-import "./src/styles/scroll.scss"
-
-export const wrapPageElement = ({ element, props }) => {
-  return <Layout {...props}>{element}</Layout>
-}
