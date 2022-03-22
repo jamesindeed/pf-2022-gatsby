af7f10f60586049f6bf68f6bae40ddb5e6e65e13
James Troughton <jamesstroughton@gmail.com>
2022-03-22 17:56:22 +1300

    ✨ Loader

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,30 @@
-import React from "react"
+import React, { useRef, useState, useEffect } from "react"
 import { Helmet } from "react-helmet"
 import "../styles/globals.scss"
 import "../styles/scroll.scss"
+import PropTypes from "prop-types"
 import CustomCursor from "../CustomCursor"
 import SmoothScroll from "../hooks/smoothScroll"
+import { MobileMessage, Loader } from "../components"
+import { gsap, Power4 } from "gsap"
 
 const Layout = ({ children, location }) => {
+  const [loaded, setLoaded] = useState(false)
+
+  let tl = gsap.timeline()
+  let sectionContainer = useRef(null)
+
+  // Transition moved to Hero component
+  // useEffect(() => {
+  //   if (loaded && sectionContainer) {
+  //     tl.fromTo(
+  //       sectionContainer,
+  //       { opacity: 0 },
+  //       { opacity: 1, ease: Power4.easeInOut, duration: 1 }
+  //     )
+  //   }
+  // }, [tl, loaded, sectionContainer])
+
   return (
     <>
       {/* //TODO: REPLACE WITH HEAD COMPONENT */}
@@ -28,11 +47,24 @@ const Layout = ({ children, location }) => {
           rel="stylesheet"
         />
       </Helmet>
-      <SmoothScroll callbacks={location} />
-      <CustomCursor />
-      <main style={{ margin: "0px 0vw" }}>{children}</main>
+
+      {loaded ? (
+        <>
+          <SmoothScroll callbacks={location} />
+          <CustomCursor />
+          <MobileMessage location={location.pathname} />
+          <main ref={el => (sectionContainer = el)} key={location.pathname}>
+            {children}
+          </main>
+        </>
+      ) : (
+        <Loader setLoaded={setLoaded} />
+      )}
     </>
   )
+}
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
 }
 
 export default Layout
