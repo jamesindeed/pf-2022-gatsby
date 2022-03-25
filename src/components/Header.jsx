d2349618485b09a7564b8798725d7be5e7b3282e
James Troughton <jamesstroughton@gmail.com>
2022-03-26 10:37:04 +1300

    💄 Style Changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,9 +8,10 @@ const Header = () => {
       <div className="navbar-container" data-scroll>
         <small>menu</small>
 
-        <img src={Logo} alt="logo" className="navbar-logo">
-          {/* J|T */}
-        </img>
+        {/* <img src={Logo} alt="logo" className="navbar-logo" /> */}
+
+        <div className="navbar-logo">J|T</div>
+        {/* <div className="navbar-logo">SOFTWARE ENGINEER</div> */}
 
         <small>work</small>
       </div>
