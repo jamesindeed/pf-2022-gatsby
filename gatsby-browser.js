8a6ff86dcf0f586df6cc98f43fe65aa2fb910f90
James Troughton <jamesstroughton@gmail.com>
2022-04-20 17:00:07 +1200

    ✨ IO

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,6 +2,13 @@ import React from "react"
 import { Layout } from "./src/components"
 import "./src/styles/scroll.scss"
 
+export const onClientEntry = () => {
+  // IntersectionObserver polyfill for gatsby-background-image (Safari, IE)
+  if (!(`IntersectionObserver` in window)) {
+    return import(`intersection-observer`)
+  }
+}
+
 export const wrapPageElement = ({ element, props }) => {
   return <Layout {...props}>{element}</Layout>
 }
