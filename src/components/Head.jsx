84a82fdc3cdbc75bb059402a432fbf9118b8b29c
James Troughton <jamesstroughton@gmail.com>
2022-04-20 17:00:44 +1200

    ✨ Head

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -1,7 +1,79 @@
 import React from "react"
+import PropTypes from "prop-types"
+import { Helmet } from "react-helmet"
+import { useLocation } from "@reach/router"
+import { useStaticQuery, graphql } from "gatsby"
 
-const Head = () => {
-  return <div>Head</div>
+const Head = ({ title, description, image }) => {
+  const { pathname } = useLocation()
+
+  const { site } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            defaultTitle: title
+            defaultDescription: description
+            siteUrl
+          }
+        }
+      }
+    `
+  )
+
+  const { defaultTitle, defaultDescription, siteUrl } = site.siteMetadata
+
+  const seo = {
+    title: title || defaultTitle,
+    description: description || defaultDescription,
+    url: `${siteUrl}${pathname}`,
+  }
+
+  return (
+    <Helmet
+      title={title}
+      defaultTitle={seo.title}
+      titleTemplate={`%s | ${defaultTitle}`}
+    >
+      <meta
+        name="viewport"
+        content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+      />
+      <html lang="en" />
+
+      <meta name="msapplication-TileColor" content="#f5f5f5" />
+
+      <meta name="description" content={seo.description} />
+
+      <meta property="og:title" content={seo.title} />
+      <meta property="og:description" content={seo.description} />
+      <meta property="og:url" content={seo.url} />
+      <meta property="og:type" content="website" />
+
+      <link
+        href="https://fonts.googleapis.com/css2?family=Syncopate:wght@400;700&display=swap"
+        rel="stylesheet"
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Bai+Jamjuree:wght@100;500;600;700&display=swap"
+        rel="stylesheet"
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Bodoni+Moda:wght@100;700&display=swap"
+        rel="stylesheet"
+      />
+    </Helmet>
+  )
 }
 
 export default Head
+
+Head.propTypes = {
+  title: PropTypes.string,
+  description: PropTypes.string,
+}
+
+Head.defaultProps = {
+  title: null,
+  description: null,
+}
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,12 @@
-import React, { useRef, useState, useEffect } from "react"
-import { Helmet } from "react-helmet"
+import React, { useRef, useState } from "react"
 import "../styles/globals.scss"
 import "../styles/scroll.scss"
 import PropTypes from "prop-types"
 import CustomCursor from "../CustomCursor"
 import SmoothScroll from "../hooks/smoothScroll"
 import { MobileMessage, Loader } from "../components"
-import { gsap, Power4 } from "gsap"
+import { gsap } from "gsap"
+import Head from "./Head"
 
 const Layout = ({ children, location }) => {
   const [loaded, setLoaded] = useState(false)
@@ -28,25 +28,7 @@ const Layout = ({ children, location }) => {
   return (
     <>
       {/* //TODO: REPLACE WITH HEAD COMPONENT */}
-      <Helmet>
-        <title>James Troughton</title>
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Syncopate:wght@400;700&display=swap"
-          rel="stylesheet"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Bai+Jamjuree:wght@100;500;600;700&display=swap"
-          rel="stylesheet"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Bodoni+Moda:wght@100;700&display=swap"
-          rel="stylesheet"
-        />
-      </Helmet>
+      <Head />
 
       {loaded ? (
         <>
